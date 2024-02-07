61b3ccf1054da95519b89b54cf030b6856c882f7
10thcode <attabadukwabena@gmail.com>
2024-02-07 06:25:49 -0800

    Add 4-app.py

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Creates a Flask application with babel i18n.
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """
+    Sets Babel default configuration
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """
+    Handles / uri.
+    """
+    return (render_template("3-index.html"))
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Gets the best matched language
+    """
+    query_string = request.query_string.decode("utf-8")
+    language = query_string[(query_string.find("=") + 1):]
+
+    if (language in app.config["LANGUAGES"]):
+        return (query_string[(query_string.find("=") + 1):])
+
+    return (request.accept_languages.best_match(app.config["LANGUAGES"]))
