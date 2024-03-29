b3c68980074cec1ac8082b90bcd493e4c55b95f6
10thcode <attabadukwabena@gmail.com>
2024-03-29 14:58:07 +0000

    Add 0-basic_cache.py

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Defines BasicCache class
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    A basic caching system
+    """
+    def __init__(self):
+        """
+        Initialize
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        Assign an item to a dictionary
+        """
+        if key is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Gets a value from a dictionary
+        """
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
